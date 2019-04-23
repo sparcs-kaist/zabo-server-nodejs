8ea4fb85375bf8a66576a34064511028241fd9ab
jungdj <jung_dj@kaist.ac.kr>
2019-04-23 11:02:50 +0900

    feat(ES6): Update app.js with ES6 syntax
    Entry point is changed from bin/www_es6 to index.js in root
    ES6 syntax can be used in bin/www
    
    TODO:
        - GraphQL
        - Mongoose
        - Authentication

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+import path from "path"
+import createError from "http-errors"
+import express from "express"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+import indexRouter from "./routes"
 
-var app = express()
+const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
@@ -16,7 +15,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,4 +33,6 @@ app.use(function (err, req, res, next) {
 	})
 })
 
-module.exports = app
+app.set('port', 3000)
+
+export default app
