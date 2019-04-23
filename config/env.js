9aeb3cbd52cba07042f7d623cef11a1f6f1522fe
jungdj <jung_dj@kaist.ac.kr>
2019-04-23 11:19:10 +0900

    feat(dotenv): Load environment variables with dotenv
    Dotenv configured in config/env.js
    
    .env loaded in every node environments
    .env.NODE_ENV loaded for specific environments
    environemtns listed in .env.example must be contained

---
+++ config/env.js
@@ -0,0 +1,21 @@
+const NODE_ENV = process.env.NODE_ENV || 'development'
+const envExpand = require('dotenv-expand')
+const envSafe = require('dotenv-safe')
+const fs = require('fs')
+
+if (!NODE_ENV) {
+    throw new Error('The NODE_ENV variable is required but was not specified.')
+}
+
+const dotenvFiles = [ 'config/.env', `config/.env.${NODE_ENV}` ].filter(Boolean)
+console.log({ dotenvFiles })
+
+dotenvFiles.forEach(dotenvFile => {
+    if (fs.existsSync(dotenvFile)) {
+        envExpand(envSafe.config({
+            path: dotenvFile,
+            example: 'config/.env.example',
+            allowEmptyValues: true,
+        }))
+    }
+})
