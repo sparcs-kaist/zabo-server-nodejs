502c0b40f3b632d20967f59f2170d09ac71eb858
cookie <jung_dj@kaist.ac.kr>
2019-05-27 16:26:33 +0900

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,12 @@ module.exports = {
 			"script": "yarn start",
 			"watch": true,
 			"ignore_watch": ["node_modules"],
+			"log_file": "combined.outerr.log",
+			"out_file": "out.log",
+			"error_file": "err.log",
+			"merge_logs": true,
+			"instances" : "max",
+			"exec_mode" : "cluster",
 			"env": {
 				"NODE_ENV": "development",
 			},
