ce35d78d3dcd95444861f949d1cd421b8c297c73
cookie <jung_dj@kaist.ac.kr>
2019-05-30 13:28:27 +0900

    separate controller and router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,52 +1,12 @@
 import express from "express"
-import SSOClient from "../utils/sso"
 
 const router = express.Router()
 
-router.get('/login', function (req, res, next) {
-	const { url, state } = SSOClient.getLoginParams();
-	console.log({
-		url,
-		state
-	})
-	req.session.state = state; // state 값을 session에 저장합니다.
-	res.redirect(url); // 사용자를 loginUrl로 redirect 시킵니다.
-	//res.render('index', { title: 'Express' })
-})
+import * as authControllers from "../controllers/auth"
 
-
-router.get('/login/callback', async (req, res) => {
-	try {
-		console.log("login callback ", req.query)
-		const stateBefore = req.session.state
-		const { state, code } = req.query
-		console.log(state, code)
-
-		if (stateBefore !== state) {
-			throw new Error('TOKEN MISMATCH: session might be hijacked!');
-		}
-
-		const userData = await SSOClient.getUserInfo(code)
-		console.log({
-			userData,
-			type: typeof userData
-		})
-		res.json(userData)
-	} catch (err) {
-		console.error(err)
-		res.sendStatus(401)
-	}
-})
-
-router.get('/logout', async (req, res) => {
-	const sid = req.session.sid
-	const redirectUrl = ''
-	const logoutUrl = SSOClient.getLogoutUrl(sid)
-	res.redirect(logoutUrl)
-})
-
-router.get('/unregister', (req, res) => {
-	res.json()
-})
+router.get('/login', authControllers.login)
+router.get('/login/callback', authControllers.loginCallback)
+router.get('/logout', authControllers.logout)
+router.get('/unregister', authControllers.unregister)
 
 module.exports = router
