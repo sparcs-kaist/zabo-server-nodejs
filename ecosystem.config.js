f7aee115af75355a7580253aa2a6719c23b4dd89
jungdj <jung_dj@kaist.ac.kr>
2019-07-04 22:42:42 +0900

    fix ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,6 @@ module.exports = {
 			"name": "zabo-server",
 			"script": "./index.js",
 			"watch": true,
-			"ignore_watch": ["node_modules"],
-			"log_file": "~/.pm2/logs/zabo-server-combined.log",
-			//"instances" : "max",
-			//"exec_mode" : "cluster",
-			//"merge_logs": true,
 			"env": {
 				"NODE_ENV": "development",
 			},
