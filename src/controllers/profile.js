33cdee425c978e8fb4f1abf71c96174fc8eb7fc6
jungdj <jung_dj@kaist.ac.kr>
2020-01-31 00:38:55 +0900

    feat(follow): Implement follow

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,12 +1,15 @@
 import ash from 'express-async-handler';
-import { User, Group, Board } from '../db';
+import { Board, Follow, Zabo } from '../db';
 
 export const getProfile = ash (async (req, res) => {
-  const { name } = req.params;
-  const [user, group] = await Promise.all ([
-    User.findOne ({ username: name }),
-    Group.findOne ({ name }),
-  ]);
+  const { user, group } = req;
+  if (group) {
+    const zabosCount = await Zabo.count ({ owner: group._id });
+    return res.json ({
+      ...group.toJSON ({ virtuals: true }),
+      zabosCount,
+    });
+  }
   if (user) {
     const result = await user
       .populate ('groups')
@@ -16,21 +19,55 @@ export const getProfile = ash (async (req, res) => {
       .populate ('likes')
       .execPopulate ();
 
+    const { groups } = user.toJSON ({ virtuals: true });
+    const counts = await Zabo.aggregate ([
+      { $match: { owner: { $in: groups.map (group => group._id) } } },
+      { $group: { _id: '$owner', count: { $sum: 1 } } },
+    ]);
+    for (let i = 0; i < groups.length; i += 1) {
+      const count = counts.find (count => groups[i]._id.equals (count._id));
+      groups[i].zabosCount = count ? count.count : 0;
+    }
+
     const [boardId] = user.boards;
     const board = await Board.findById (boardId);
 
-    const likesCount = result.likes.length;
+
     const pinsCount = board.pins.length;
     return res.json ({
-      ...result.toJSON (),
-      likesCount,
+      ...result.toJSON ({ virtuals: true }),
+      groups,
       pinsCount,
     });
   }
-  if (group) {
-    return res.json (group);
+  throw new Error ('Cannot reach here');
+});
+
+export const followController = ash (async (req, res) => {
+  const { self, user, profile } = req;
+  const onModel = user ? 'User' : 'Group';
+  const prevFollow = profile.followers.find (follower => follower.equals (self._id));
+  if (prevFollow) {
+    self.followings = self.followings.filter (follow => follow.equals (prevFollow._id));
+    profile.followers = profile.followers.filter (follower => follower.equals (prevFollow._id));
+    await Promise.all ([
+      Follow.deleteOne ({ _id: prevFollow._id }),
+      self.followings.pull ({ _id: prevFollow._id }),
+      profile.followers.pull ({ _id: prevFollow._id }),
+    ]);
+    return res.send ({
+      following: false,
+    });
   }
-  return res.status (404).json ({
-    error: 'user and group with given name does not exist',
+  const follow = await Follow.create ({
+    followee: profile._id,
+    follower: self._id,
+    onModel,
+  });
+  profile.followers.push (follow._id);
+  self.followings.push (follow._id);
+  await Promise.all ([profile.save (), self.save ()]);
+  return res.send ({
+    following: true,
   });
 });
