7a5657a9879e0ee6152200dbfff93581089ab554
jungdj <jung_dj@kaist.ac.kr>
2020-03-01 07:25:41 +0900

    feat(Signale): Add signale

--- src/bin/worker.js
+++ src/bin/worker.js
@@ -1,7 +1,13 @@
 import cron from 'node-cron';
 import { updateRecommends, updateScores } from '../worker/recommender';
+import signale from '../utils/logger/signale';
+
+const zaboSignale = signale.scope ('zabo');
+const userSignale = signale.scope ('user');
 
 cron.schedule ('*/17 * * * *', () => {
+  const hrstart = process.hrtime ();
+  zaboSignale.info ('ZABO Scoring start');
   updateScores ()
     .then (() => {
       console.log ('success');
@@ -9,10 +15,16 @@ cron.schedule ('*/17 * * * *', () => {
     .catch (error => {
       console.error (error);
       console.log ('failure');
+    })
+    .finally (() => {
+      const hrend = process.hrtime (hrstart);
+      zaboSignale.info ('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
     });
 });
 
 cron.schedule ('43 */19 * * * *', () => {
+  const hrstart = process.hrtime ();
+  userSignale.info ('User Scoring start');
   updateRecommends ()
     .then (() => {
       console.log ('success');
@@ -20,5 +32,9 @@ cron.schedule ('43 */19 * * * *', () => {
     .catch (error => {
       console.error (error);
       console.log ('failure');
+    })
+    .finally (() => {
+      const hrend = process.hrtime (hrstart);
+      userSignale.info ('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
     });
 });
