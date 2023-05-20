f3a14c9175370b668df14bee172795d50c5355a6
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-21 00:15:13 +0900

    feature: delete unused variable

--- src/admin/actions/uploadZabo.js
+++ src/admin/actions/uploadZabo.js
@@ -1,11 +1,8 @@
-import { adminZaboGroup } from "..";
-
 export const uploadZaboAction = {
   actionType: "resource",
   component: false,
   handler: async (req, res, context) => {
     const currentAdmin = req.adminUser;
-    const adminGroup = adminZaboGroup;
 
     return [];
   },
