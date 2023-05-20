e0828adb633d15271db506261a99bb573d071db8
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-21 00:16:12 +0900

    feature: create upload zabo action with component

--- src/admin/resources/Zabo.js
+++ src/admin/resources/Zabo.js
@@ -1,11 +1,25 @@
 import { Zabo } from "../../db";
 import { uploadZaboAction } from "../actions/uploadZabo";
 import { deleteZaboAction } from "../actions/deleteZabo";
+import { Components } from "../components";
 export const ZaboResource = {
   resource: Zabo,
   options: {
     actions: {
-      uploadZabo: uploadZaboAction,
+      uploadZabo: {
+        actionType: "resource",
+        component: Components.uploadZaboComponent,
+        handler: (req, res, context) => {
+          console.log(req);
+          console.log("upload zabo action called");
+          return [
+            {
+              record: "hello",
+              msg: "hello world!",
+            },
+          ];
+        },
+      },
       deleteZabo: deleteZaboAction,
     },
   },
