0bff1bb0dc2c2f750d7cdd848210fd83abc07160
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-21 00:40:10 +0900

    feauture: delete dummy action

--- src/admin/resources/Zabo.js
+++ src/admin/resources/Zabo.js
@@ -6,20 +6,7 @@ export const ZaboResource = {
   resource: Zabo,
   options: {
     actions: {
-      uploadZabo: {
-        actionType: "resource",
-        component: Components.uploadZaboComponent,
-        handler: (req, res, context) => {
-          console.log(req);
-          console.log("upload zabo action called");
-          return [
-            {
-              record: "hello",
-              msg: "hello world!",
-            },
-          ];
-        },
-      },
+      uploadZabo: uploadZaboAction,
       deleteZabo: deleteZaboAction,
     },
   },
