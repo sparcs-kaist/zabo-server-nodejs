0778882b7a6e957fb1d9c756ad365e508ad918d8
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-28 02:28:52 +0900

    feature:preprocess images and attach to formData

--- src/admin/components/uploadZabo.jsx
+++ src/admin/components/uploadZabo.jsx
@@ -1,11 +1,86 @@
 import React, {useState} from 'react'
 import { Box, H3, Input, Button } from '@adminjs/design-system';
+import axios from 'axios';
 
+const gridLayoutCompareFunction = (a, b) => {
+  const { x: ax, y: ay } = a.updatedLayout;
+  const { x: bx, y: by } = b.updatedLayout;
+  if (ay - by) return ay - by;
+  return ax - bx;
+};
+
+const loadImageFile = (file) => {
+  console.log("start loadimagefile function");
+  const img = new Image();
+  const objectUrl = URL.createObjectURL(file);
+  return new Promise((res) => {
+    img.onload = () => {
+      res(img);
+    };
+    img.src = objectUrl;
+  });
+};
+
+const cropImage = async (file, ratio) => {
+  const image = await loadImageFile(file);
+  const { width, height } = image;
+  const ownRatio = width / height;
+  let dWidth;
+  let dHeight;
+  const canvas = document.createElement("canvas");
+  if (ownRatio > ratio) {
+    dHeight = height;
+    dWidth = height * ratio;
+  } else {
+    dWidth = width;
+    dHeight = width / ratio;
+  }
+  canvas.width = dWidth;
+  canvas.height = dHeight;
+  const context = canvas.getContext("2d");
+  if (!context) return "";
+  context.fillStyle = "white";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // crop it top center
+  context.drawImage(image, 0, 0, dWidth, dHeight, 0, 0, dWidth, dHeight);
+  // const base64ImageData = canvas.get(0).toDataURL();
+  // const imgSrc = canvas.toDataURL ('image/png');
+  return canvas.toDataURL("image/jpeg");
+};
+
+const dataURLToBlob = (dataURL) => {
+  const blobBin = atob(dataURL.split(",")[1]);
+  const array = [];
+  for (let i = 0; i < blobBin.length; i += 1) {
+    array.push(blobBin.charCodeAt(i));
+  }
+  // const file = new Blob ([new Uint8Array (array)], { type: 'image/png' });
+  return new Blob([new Uint8Array(array)]);
+};
+
+const uploadZabo = async (formData) => {
+
+  //TODO: create admin zabo upload api.
+  //existing api has jwt check middleware. So we need to create new api for admin user.
+
+  await axios.post("/api/zabo", formData, {
+    headers: {
+      "Content-Type": "multipart/form-datra",
+    }
+  })
+}
+
+const imageFileGetWidthHeight = async (file) => {
+  console.log("start imagefilegetwidthheight function");
+  const image = await loadImageFile(file);
+  console.log("finish loadimagefile function");
+  const { width, height } = image;
+  return { width, height, ratio: width / height };
+};
 
 //admin user가 외부 단체의 자보를 올려줄 때 사용하는 액션
 const uploadZaboComponent = (props) => {
-  console.log(props)
-  console.log("uploadZaboAction component")
   const [zaboList, setZaboList] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -13,33 +88,47 @@ const uploadZaboComponent = (props) => {
   const [schedule, setSchedule] = useState({});
 
   const handleZaboChange = (e) => {
-    setZaboList(e.target.files);
+    const files = Array.from(e.target.files);
+    const newZaboList = files.map((file, index) => {
+      const preview = URL.createObjectURL(file);
+      const i = files.length + index;
+      const key = `${file.name}-${i}`;
+      const layout = {
+        x: i % 5, //TODO: What cols mean
+        y: Math.floor(i / 5),
+        w: 1,
+        h: 1,
+        i: key,
+      }
+
+      return Object.assign(file, {
+        preview,
+        key,
+        layout,
+        updatedLayout: layout,
+      });
+    });
+    setZaboList(newZaboList);
   }
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
-    console.log(title)
   }
 
   const handleDescriptionChange = (e) => {
     setDescription(e.target.value);
-    console.log(description)
   }
 
   const handleCategoryChange = (e) => {
     setCategory(e.target.value);
-    console.log(category)
   }
 
 
-  const handleZaboUpload = () => {
+  const handleZaboUpload = async () => {
     if(!zaboList) {
       //TODO: show error message that there is no file to upload 
       return;
     }
-
-    console.log("uploading zabo to server!");
-    console.log(zaboList)
 
     const formData = new FormData();
     formData.append("title", title);
@@ -48,10 +137,34 @@ const uploadZaboComponent = (props) => {
     //TODO: handle if schedule exists
     // formData.append("schedule", schedule);
 
-    //don't use zabo post api. implement within this function
-    //because circumstances are different
+    //console log formdata
+    for (var key of formData.entries()) {
+      console.log(key[0] + ', ' + key[1]);
+    }
 
+    //resize images
+    const sortedImageFiles = await zaboList.slice()
+    await sortedImageFiles.sort(gridLayoutCompareFunction);
     
+    console.log(sortedImageFiles);
+
+    const {width, height} = await imageFileGetWidthHeight(sortedImageFiles[0]);
+    console.log("finish getwidthheight function");
+    console.log(`width: ${width}, height: ${height}`);
+    const ratio = width / height;
+    if (ratio > 2) ratio = 2;
+    else if (ratio < 0.5) ratio = 0.5;
+    const sources = await Promise.all(sortedImageFiles.map((file) => cropImage(file, ratio)));
+    sources.forEach((imgSrc) => {
+      const blob = dataURLToBlob(imgSrc);
+      formData.append("img", blob);
+    })
+
+    //console log formdata
+    for (var key of formData.entries()) {
+      console.log(key[0] + ', ' + key[1]);
+    }
+    await uploadZabo(formData);
     return
   }
   
@@ -78,3 +191,13 @@ const uploadZaboComponent = (props) => {
 };
 
 export default uploadZaboComponent;
+
+//admin flow for uploading zabo
+
+//1. admin user clicks upload zabo button
+
+//2. admin user fills out the form: title, description, category, schedule, zabo files
+
+//3. The image validation check(size check) is done. And create form data, send API request to server.
+
+//4. Send API request to server with form data
