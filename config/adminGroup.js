af7f669ad0a34e5224ebf69f34df5f1b6b17d0d5
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-30 00:06:38 +0900

    feature: add admin group config

---
+++ config/adminGroup.js
@@ -0,0 +1,8 @@
+export const adminZaboGroup = {
+    name: "관리자 그룹",
+    description: "관리자 그룹",
+    subtitle: "관리자 그룹",
+    purpose: "관리자 그룹",
+    category: "관리자그룹",
+    isBusiness: false,
+  };
