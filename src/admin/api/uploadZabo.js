3d3bbc2bd8754dc078a824170119488a1f909673
CHOI <ball@CHOIs-MacBook-Air.local>
2023-06-09 12:13:41 +0900

    bug: circular dependency problem occur when using aws s3 instance

--- src/admin/api/uploadZabo.js
+++ src/admin/api/uploadZabo.js
@@ -1,11 +1,11 @@
-import { logger } from "../../utils/logger";
-//import {s3, bucket} from '../../utils/aws';
+//import { logger } from "../../utils/logger";
+//import {s3} from '../../utils/aws';
 
 const uploadZabo = async zaboForm => {
   //const adminName = currentAdmin.user.username;
   const adminName = "testadmin";
 
-  logger.admin.info("Upload Zabo; adminName: %s", adminName);
+  //logger.admin.info("Upload Zabo; adminName: %s", adminName);
   const {
     title,
     description,
@@ -37,65 +37,67 @@ const uploadZabo = async zaboForm => {
   let params;
 
   //use aws sdk s3 to upload image to aws s3 bucket
-  // for(let i = 0 ; i < files.length ; i += 1) {
-  //     const filename = `${zaboCount}${Date.now().toString()}`;
-  //     params = {
-  //         Bucket: bucket,
-  //         Key: `zabo/zabo-${filename}${path.extname('blob')}`,
-  //         Body: files[i],
-  //     }
-
-  //     console.log("uploading image to s3");
-  // s3.upload(params, (err, data) => {
-  //     if (err) {
-  //         console.log(err);
-  //     }
-  //     else {
-  //         console.log(data);
-  //     }
-  // });
-  //}
-
-  const newZabo = new Zabo({
-    owner: currentAdmin.currentGroup,
-    createdBy: currentAdmin._id,
-    title,
-    description,
-    category,
-    schedules,
-  });
-
-  const calSizes = [];
   for (let i = 0; i < files.length; i += 1) {
-    const s3ImageKey = files[i].key;
-    calSizes.push(sizeS3Item(s3ImageKey));
+    const filename = `${zaboCount}${Date.now().toString()}`;
+    console.log(files[i]);
+    params = {
+      Bucket: bucket,
+      Key: `zabo/zabo-${filename}${path.extname("blob")}`,
+      Body: files[i],
+    };
+
+    console.log("uploading image to s3");
+    console.log(params);
+    // s3.upload(params, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     else {
+    //         console.log(data);
+    //     }
+    // });
   }
 
-  const results = await Promise.all(calSizes);
+  // const newZabo = new Zabo({
+  //   owner: currentAdmin.currentGroup,
+  //   createdBy: currentAdmin._id,
+  //   title,
+  //   description,
+  //   category,
+  //   schedules,
+  // });
 
-  const photos = results.map(([dimensions, bytesRead], index) => ({
-    url: files[index].location,
-    width: dimensions.width,
-    height: dimensions.height,
-  }));
+  // const calSizes = [];
+  // for (let i = 0; i < files.length; i += 1) {
+  //   const s3ImageKey = files[i].key;
+  //   calSizes.push(sizeS3Item(s3ImageKey));
+  // }
 
-  newZabo.photos = newZabo.photos.concat(photos);
-  await Promise.all([
-    newZabo.save(),
-    Group.findByIdAndUpdate(currentAdmin.currentGroup, {
-      $set: { recentUpload: new Date() },
-    }),
-  ]);
+  // const results = await Promise.all(calSizes);
 
-  await newZabo
-    .populate("owner", "name profilePhoto subtitle description")
-    .execPopulate();
+  // const photos = await results.map(([dimensions, bytesRead], index) => ({
+  //   url: files[index].location,
+  //   width: dimensions.width,
+  //   height: dimensions.height,
+  // }));
 
-  const zaboJSON = newZabo.toJSON();
-  zaboJSON.isLiked = false;
-  zaboJSON.isPinned = false;
+  // newZabo.photos = newZabo.photos.concat(photos);
+  // await Promise.all([
+  //   newZabo.save(),
+  //   Group.findByIdAndUpdate(currentAdmin.currentGroup, {
+  //     $set: { recentUpload: new Date() },
+  //   }),
+  // ]);
 
-  return zaboJSON;
+  // await newZabo
+  //   .populate("owner", "name profilePhoto subtitle description")
+  //   .execPopulate();
+
+  // const zaboJSON = newZabo.toJSON();
+  // zaboJSON.isLiked = false;
+  // zaboJSON.isPinned = false;
+
+  // return zaboJSON;
 };
 
 export default uploadZabo;
