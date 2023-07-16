2b5611dd4c98085b01e4bf24dd6525405b0da81d
CHOI <ball@CHOIs-MacBook-Air.local>
2023-07-17 01:29:36 +0900

    feat: create zabo upload action

--- src/admin/actions/uploadZabo.js
+++ src/admin/actions/uploadZabo.js
@@ -1,8 +1,15 @@
-import { Components } from "../components";
+import uploadZabo from "../api/uploadZabo";
 
 //action without component.
 //component will use axios to send request for uploading zabo
 export const uploadZaboAction = {
   actionType: "resource",
-  component: Components.uploadZaboComponent,
+  component: false,
+  guard: "Do you really want to upload this zabo?",
+  handler: async (req, res, context) => {
+    console.log(req);
+    const currentAdmin = req.adminUser;
+    const zaboJson = await uploadZabo(currentAdmin, req.body, req.files);
+    res.json(zaboJson);
+  },
 };
