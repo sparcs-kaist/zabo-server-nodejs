4567c199fe586e35eedae3a01031317e50f203ff
CHOI <ball@CHOIs-MacBook-Air.local>
2023-07-17 01:32:21 +0900

    feat: add zabo upload page

--- src/admin/resources/Zabo.js
+++ src/admin/resources/Zabo.js
@@ -1,12 +1,13 @@
 import { Zabo } from "../../db";
 import { uploadZaboAction } from "../actions/uploadZabo";
 import { deleteZaboAction } from "../actions/deleteZabo";
-import { Components } from "../components";
+import { uploadZaboPage } from "../actions/uploadZaboPage";
 export const ZaboResource = {
   resource: Zabo,
   options: {
     actions: {
       uploadZabo: uploadZaboAction,
+      uploadZaboPage: uploadZaboPage,
       deleteZabo: deleteZaboAction,
     },
   },
