5b67178b0de0b44515e87544581ceff054ba2653
CHOI <ball@CHOIs-MacBook-Air.local>
2023-07-17 01:30:33 +0900

    refactor: refactor structure

--- src/admin/api/uploadZabo.js
+++ src/admin/api/uploadZabo.js
@@ -1,18 +1,13 @@
-//import { logger } from "../../utils/logger";
-//import {s3} from '../../utils/aws';
+import { logger } from "../../utils/logger";
+import { s3 } from "../../utils/aws";
 
-const uploadZabo = async zaboForm => {
-  //const adminName = currentAdmin.user.username;
-  const adminName = "testadmin";
+const uploadZabo = async (currentAdmin, zaboForm, zaboFiles) => {
+  const adminName = currentAdmin.user.username;
 
-  //logger.admin.info("Upload Zabo; adminName: %s", adminName);
-  const {
-    title,
-    description,
-    schedules: jsonSchedules,
-    category,
-    files,
-  } = zaboForm;
+  logger.admin.info("Upload Zabo; adminName: %s", adminName);
+
+  const { title, description, schedules: jsonSchedules, category } = zaboForm;
+
   const schedules = parseJSON(jsonSchedules, []);
 
   category = (category || "")
@@ -37,67 +32,66 @@ const uploadZabo = async zaboForm => {
   let params;
 
   //use aws sdk s3 to upload image to aws s3 bucket
-  for (let i = 0; i < files.length; i += 1) {
+  for (let i = 0; i < zaboFiles.length; i += 1) {
     const filename = `${zaboCount}${Date.now().toString()}`;
-    console.log(files[i]);
+    console.log(zaboFiles[i]);
     params = {
       Bucket: bucket,
       Key: `zabo/zabo-${filename}${path.extname("blob")}`,
-      Body: files[i],
+      Body: zaboFiles[i],
     };
 
     console.log("uploading image to s3");
     console.log(params);
-    // s3.upload(params, (err, data) => {
-    //     if (err) {
-    //         console.log(err);
-    //     }
-    //     else {
-    //         console.log(data);
-    //     }
-    // });
+    s3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
   }
 
-  // const newZabo = new Zabo({
-  //   owner: currentAdmin.currentGroup,
-  //   createdBy: currentAdmin._id,
-  //   title,
-  //   description,
-  //   category,
-  //   schedules,
-  // });
+  const newZabo = new Zabo({
+    owner: currentAdmin.currentGroup,
+    createdBy: currentAdmin._id,
+    title,
+    description,
+    category,
+    schedules,
+  });
 
-  // const calSizes = [];
-  // for (let i = 0; i < files.length; i += 1) {
-  //   const s3ImageKey = files[i].key;
-  //   calSizes.push(sizeS3Item(s3ImageKey));
-  // }
+  const calSizes = [];
+  for (let i = 0; i < zaboFiles.length; i += 1) {
+    const s3ImageKey = zaboFiles[i].key;
+    calSizes.push(sizeS3Item(s3ImageKey));
+  }
 
-  // const results = await Promise.all(calSizes);
+  const results = await Promise.all(calSizes);
 
-  // const photos = await results.map(([dimensions, bytesRead], index) => ({
-  //   url: files[index].location,
-  //   width: dimensions.width,
-  //   height: dimensions.height,
-  // }));
+  const photos = await results.map(([dimensions, bytesRead], index) => ({
+    url: files[index].location,
+    width: dimensions.width,
+    height: dimensions.height,
+  }));
 
-  // newZabo.photos = newZabo.photos.concat(photos);
-  // await Promise.all([
-  //   newZabo.save(),
-  //   Group.findByIdAndUpdate(currentAdmin.currentGroup, {
-  //     $set: { recentUpload: new Date() },
-  //   }),
-  // ]);
+  newZabo.photos = newZabo.photos.concat(photos);
+  await Promise.all([
+    newZabo.save(),
+    Group.findByIdAndUpdate(currentAdmin.currentGroup, {
+      $set: { recentUpload: new Date() },
+    }),
+  ]);
 
-  // await newZabo
-  //   .populate("owner", "name profilePhoto subtitle description")
-  //   .execPopulate();
+  await newZabo
+    .populate("owner", "name profilePhoto subtitle description")
+    .execPopulate();
 
-  // const zaboJSON = newZabo.toJSON();
-  // zaboJSON.isLiked = false;
-  // zaboJSON.isPinned = false;
+  const zaboJSON = newZabo.toJSON();
+  zaboJSON.isLiked = false;
+  zaboJSON.isPinned = false;
 
-  // return zaboJSON;
+  return zaboJSON;
 };
 
 export default uploadZabo;
