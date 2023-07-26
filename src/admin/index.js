45af65992e765863109178e018979de4466de212
root <root@Docker-server.8.8.8.8>
2023-07-26 10:06:17 +0000

    refactor: remove duplicated admin group config

--- src/admin/index.js
+++ src/admin/index.js
@@ -42,12 +42,3 @@ const adminOptions = {
 const admin = new AdminJS(adminOptions);
 export const adminRouter = AdminJSExpress.buildRouter(admin);
 export const adminRouterPath = admin.options.rootPath;
-
-export const adminGroupInfo = {
-  name: "관리자 그룹",
-  description: "admin group",
-  subtitle: "admin group",
-  purpose: "admin group",
-  category: "admin",
-  isBusiness: false,
-};
