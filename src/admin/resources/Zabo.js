7ef1ed79575ce2d760946efad83ec21daff5c44a
root <root@Docker-server.8.8.8.8>
2023-07-26 13:52:56 +0000

    refactor: remove unused action

--- src/admin/resources/Zabo.js
+++ src/admin/resources/Zabo.js
@@ -1,12 +1,10 @@
 import { Zabo } from "../../db";
-import { uploadZaboAction } from "../actions/uploadZabo";
 import { deleteZaboAction } from "../actions/deleteZabo";
 import { uploadZaboPage } from "../actions/uploadZaboPage";
 export const ZaboResource = {
   resource: Zabo,
   options: {
     actions: {
-      uploadZabo: uploadZaboAction,
       uploadZaboPage: uploadZaboPage,
       deleteZabo: deleteZaboAction,
     },
